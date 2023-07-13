cc37c9131d021acf91badb793a1ea02cb492322e
Sung Hoon Jung <sunghjung3@utexas.edu>
2023-07-13 10:55:47 -0500

    modified min_ppp.py to use libufmin_analysis.py

--- scripts/ufmin/analysis/min_ppp.py
+++ scripts/ufmin/analysis/min_ppp.py
@@ -16,94 +16,13 @@ from uf3.forcefield import calculator
 from uf3.regression import least_squares
 from uf3.util import cubehelix
 
+from libufmin_analysis import plot_pair_energy
+
 
 def lj(r_min, depth, r):
     sigma = r_min*(2 ** (-1/6))
     a = (sigma / r) ** 6
     return 4*depth * ( a*a - a )
-
-
-def bspline_interpolate(xs, c, kn):
-    # x, coefficient for basis, knot sequence for this basis
-    kno = np.concatenate([np.repeat(kn[0], 3),
-                          kn,
-                          np.repeat(kn[-1], 3)])
-    bs = interpolate.BSpline(kno,                            
-                             np.array([0, 0, 0, c, 0, 0, 0]),
-                             3,
-                             extrapolate=False)
-    y_plot = bs(xs)
-    return y_plot
-
-def bsplines_interpolate(xs, coefficients, knot_sequence, sum=True):
-    basis_components = []
-    for i, c in enumerate(coefficients):
-        kn = knot_sequence[i:i + 5]
-        y_plot = bspline_interpolate(xs, c, kn)
-        y_plot[np.isnan(y_plot)] = 0
-        basis_components.append(y_plot)
-    if sum:
-        return np.sum(basis_components, axis=0)
-    else:
-        return basis_components
-
-def calc_pair_energy(rs, coefficient_1b, coefficients_2b, knot_sequence, nAtoms):
-    # includes 1 body offset energy
-    nBonds = nAtoms * (nAtoms - 1) / 2
-    return bsplines_interpolate(rs, 2*coefficients_2b, knot_sequence) + np.ones(np.shape(rs)) * nAtoms / nBonds * coefficient_1b
-
-# from uf3.util.plotting.visualize_splines()
-def plot_pair_energy(coefficient_1b, coefficients_2b, knot_sequence, nAtoms, ax=None, cmap=None, show_components=True, show_total=True, xlim=None, ylim=None):
-    if xlim is None:
-        r_min = knot_sequence[0]
-        r_max = knot_sequence[-1]
-        xlim = [r_min, r_max]
-    if ax is None:              
-        fig, ax = plt.subplots()
-    else:                    
-        fig = ax.get_figure()
-    if cmap is None:
-        cmap = cubehelix.c_rainbow                     
-    colors = cmap(np.linspace(0, 1, len(coefficients_2b + 1)))  # +1 for 1b coefficient
-    #x_plot = np.linspace(r_min, r_max, 1000)
-    x_plot = np.linspace(*xlim, 200)
-    basis_components = bsplines_interpolate(x_plot, 2*coefficients_2b, knot_sequence, sum=False)
-    if show_components:
-        for i, basis_component in enumerate(basis_components):
-            ax.plot(x_plot,
-                    basis_component,
-                    color=colors[i],
-                    linewidth=1)
-
-    y_total = np.sum(basis_components, axis=0)
-    if ylim is None:
-        s_min = np.min(y_total[~np.isnan(y_total)])
-        s_max = np.max(y_total[~np.isnan(y_total)])
-        ylim = [s_min, s_max]
-
-    # add 1 body offset
-    nBonds = nAtoms * (nAtoms - 1) / 2
-    y_plot = np.ones(np.shape(x_plot)) * nAtoms / nBonds * coefficient_1b
-    if show_components:
-        ax.plot(x_plot,
-                y_plot,
-                '--',
-                color=colors[-1],
-                linewidth=1)
-    y_total += y_plot
-
-    if show_total:
-        ax.plot(x_plot,
-                y_total,
-                c='k',      
-                linewidth=2) 
-    #ax.set_xlim(r_min, r_max)
-    #ax.set_ylim(s_min, s_max)
-    ax.set_xlim(xlim)
-    ax.set_ylim(ylim)
-    ax.set_xlabel("r")
-    ax.set_ylabel("E(r)")
-    return fig, ax
 
 
 def opt_pair_pot_frames(radial_potential, traj, model=None, training_traj=None, label='', xlim=None, ylim=None, combine=True,
