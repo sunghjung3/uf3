a854c4932573435b1de9ec070c000f5f6f7c5274
Sung Hoon Jung <sunghjung3@utexas.edu>
2023-12-07 14:57:49 -0600

    added vasp as one of true calc in ufmin.py

--- scripts/ufmin/run/ufmin.py
+++ scripts/ufmin/run/ufmin.py
@@ -1,7 +1,15 @@
 from ase.io import read
-#from ase.calculators.emt import EMT
+
+from ase.calculators.emt import EMT
+from ase.calculators.singlepoint import SinglePointCalculator
 from ase.calculators.lj import LennardJones
 from ase.calculators.morse import MorsePotential
+try:
+    from ase.calculators.vasp import Vasp
+    from pymatgen.io.vasp.outputs import Vasprun
+except ImportError:
+    Warning.warn("Trouble importing Vasp-related packages. Vasp cannot be used.")
+
 #from ase.optimize.sciopt import SciPyFminCG
 from ase.optimize import FIRE
 #from myopts import GD, MGD
@@ -63,6 +71,20 @@ class Pseudodict(dict):
     def get(self, key, default=None):
         return self.value
 
+def strip_calc(atoms, e_val, f_val):
+    """
+    Given an atoms object and energy and force values, strip the calculator
+    and replace it with a single point calculator with the same energy and force
+    values.
+    """
+    ret_atoms = copy.deepcopy(atoms)
+    ret_atoms.calc = SinglePointCalculator(ret_atoms, energy=e_val, forces=f_val)
+    return ret_atoms
+
+def check_vasp_convergence(vasprun_file="vasprun.xml") -> bool:
+    if Vasprun(vasprun_file).converged_electronic:
+        return True
+    raise ValueError("VASP calculation did not converge. Terminating UFMin.")
 
 #@profile
 def ufmin(initial_structure = "POSCAR",
@@ -83,6 +105,7 @@ def ufmin(initial_structure = "POSCAR",
           max_uf3_calls = 1000,
           verbose = 0,
           true_calc = None,
+          true_calc_type = None,
           resolution_map = None,
           learning_weight = 0.5,
           regularization_values = None,
@@ -91,6 +114,7 @@ def ufmin(initial_structure = "POSCAR",
           sample_weight_strength = 6,
           pretrained_models = None,
           normalize_forces = False,
+          true_forcecall_always = True,
           resume = 0  # start from scratch if 0. Do `resume` additional forcecalls if non-zero. If non-zero, overrides `max_forcecalls` parameter.
           ):
     """
@@ -118,6 +142,8 @@ def ufmin(initial_structure = "POSCAR",
         max_uf3_calls (int): Maximum number of force calls to make on the UF3 surface.
         verbose (int): Verbosity level.
         true_calc (ase.calculator.Calculator): ASE calculator to use for true energy and force evaluations.
+        true_calc_type (str): Type of true calculator.
+            e.g. "vasp" for VASP
         resolution_map (dict): Resolution map for features.
         learning_weight (float): Weight for learning term in loss function.
         regularization_values (list): ridge and curvature regularization values.
@@ -129,6 +155,8 @@ def ufmin(initial_structure = "POSCAR",
         pretrained_models (dict): List of pretrained models to use for training. 
             forcecall number -> model file path
         normalize_forces (bool): Whether or not to normalize features by max true force value.
+        true_forcecall_always (bool): If true, evaluates the true calculator even during sub-optimization on the UF3 surface.
+            For analysis purposes.
         resume (int): Start from scratch if 0.
     """
 
@@ -215,11 +243,19 @@ def ufmin(initial_structure = "POSCAR",
             r_cut = 8 * r_min
             well_depth = 9
             true_calc = LennardJones(sigma=r_min*(2 ** (-1/6)), epsilon=well_depth, rc=r_cut)
+            true_calc_type = 'lj'
+        print("True calc:", true_calc_type)
+        if true_calc_type == "vasp" and initial_structure == "POSCAR":
+            tmp_poscar_name = "POSCAR_" + str(time.time_ns())
+            os.rename("POSCAR", tmp_poscar_name)
         atoms.calc = true_calc
         most_recent_E_eval = atoms.get_potential_energy()
         most_recent_F_eval = atoms.get_forces()
+        if true_calc_type == "vasp":
+            check_vasp_convergence()
         pickle.dump( (most_recent_E_eval, most_recent_F_eval), true_calc_file )
-        traj.append(copy.deepcopy(atoms))
+        #traj.append(copy.deepcopy(atoms))
+        traj.append(strip_calc(atoms, most_recent_E_eval, most_recent_F_eval))
 
         # UF3 cannot train with a single structure (look at loss function). So take 1 real optimization step to gain another image
         #dyn = SciPyFminCG(atoms)
@@ -227,8 +263,11 @@ def ufmin(initial_structure = "POSCAR",
         dyn.run(steps=1)
         most_recent_E_eval = atoms.get_potential_energy()
         most_recent_F_eval = atoms.get_forces()
+        if true_calc_type == "vasp":
+            check_vasp_convergence()
         pickle.dump( (most_recent_E_eval, most_recent_F_eval), true_calc_file )
-        traj.append(copy.deepcopy(atoms))
+        #traj.append(copy.deepcopy(atoms))
+        traj.append(strip_calc(atoms, most_recent_E_eval, most_recent_F_eval))
 
     '''
     # 2-body preconditioning
@@ -378,10 +417,23 @@ def ufmin(initial_structure = "POSCAR",
             ufmin_counter += 1
 
             # compare with true energy and froces at this point
-            true_atoms = copy.deepcopy(atoms)
-            true_atoms.calc = true_calc
-            step_model_calc_E.append( (atoms.get_potential_energy(), true_atoms.get_potential_energy()) )
-            step_model_calc_F.append( (atoms.get_forces(), true_atoms.get_forces()) )
+            e_val = atoms.get_potential_energy()
+            f_val = atoms.get_forces()
+            if true_forcecall_always:
+                true_atoms = copy.deepcopy(atoms)
+                true_atoms.calc = true_calc
+                true_e_val = true_atoms.get_potential_energy()
+                true_f_val = true_atoms.get_forces()
+                if true_calc_type == "vasp":
+                    check_vasp_convergence()
+                del true_atoms[:]
+                del true_atoms
+            else:
+                true_e_val = most_recent_E_eval
+                true_f_val = most_recent_F_eval
+            step_model_calc_E.append( (e_val, true_e_val) )
+            step_model_calc_F.append( (f_val, true_f_val) )
+
 
             uf3_forces_squared = np.sum( np.square(step_model_calc_F[-1][0]), axis=1 )
             uf3_fmax_squared = np.max(uf3_forces_squared)
@@ -402,9 +454,6 @@ def ufmin(initial_structure = "POSCAR",
             uq_f_test = delta_uq.get_uncertainty(x_f_test, model.coefficients, p, scale=mae_f)
             step_uq_f_list.append(uq_f_test)
             '''
-            del true_atoms[:]
-            del true_atoms
-
             
             #if too_uncertain(traj[-1], atoms) or uf3_fmax_squared < ufmin_uf3_fmax_squared or ufmin_counter > max_uf3_calls:
             high_uncertainty = r_uq_obj.too_uncertain()
@@ -423,12 +472,15 @@ def ufmin(initial_structure = "POSCAR",
         atoms.calc = true_calc
         most_recent_E_eval = atoms.get_potential_energy()
         most_recent_F_eval = atoms.get_forces()
+        if true_calc_type == "vasp":
+            check_vasp_convergence()
         pickle.dump( (most_recent_E_eval, most_recent_F_eval), true_calc_file )
         print(forcecall_counter, " ; true E =", most_recent_E_eval)
         true_forces_squared = np.sum( np.square(most_recent_F_eval), axis=1 )
         true_fmax_squared = np.max(true_forces_squared)
         print(forcecall_counter, " ; true F =", np.sqrt(true_fmax_squared))
-        traj.append(copy.deepcopy(atoms))
+        #traj.append(copy.deepcopy(atoms))
+        traj.append(strip_calc(atoms, most_recent_E_eval, most_recent_F_eval))
 
         # explicit garbage collection
         del step_model_calc_E[:]
@@ -487,6 +539,9 @@ def ufmin(initial_structure = "POSCAR",
 
     gc.collect()
 
+    if true_calc_type == "vasp" and initial_structure == "POSCAR":
+        os.rename(tmp_poscar_name, "POSCAR")
+
 
 if __name__ == "__main__":
     ### VARIABLES ###
@@ -511,18 +566,66 @@ if __name__ == "__main__":
     verbose = 0
     pretrained_models = None
     normalize_forces = False
+    true_forcecall_always = True
     resume = 0
 
+    ### ==================== ###
+    ### TRUE CALCULATORS ###
+    # Lennard-Jones
     r_min = 2.22
     r_cut = 8 * r_min
     well_depth = 9
     true_calc = LennardJones(sigma=r_min*(2 ** (-1/6)), epsilon=well_depth, rc=r_cut)
+    true_calc_type = "lj"
 
+    # Morse
     #r_e = 2.897
     #D_e = 0.7102
     #exp_prefactor = 1.6047
     #rho0 = exp_prefactor * r_e 
     #true_calc = MorsePotential(epsilon=D_e, r0=r_e, rho0=rho0, rcut1=4.0, rcut2=7.0)
+    #true_calc_type = "morse"
+
+    # UF3
+    #true_model = least_squares.WeightedLinearModel.from_json("/home/sung/UFMin/sung/representability_test/fit_3b/true_model_3b.json")
+    #true_calc = calculator.UFCalculator(true_model)
+    #true_calc_type = "uf3"
+
+    # VASP
+    #true_calc = Vasp(prec = 'Medium',
+    #                 xc = 'PBE',
+    #                 setups = {'Pt':''},
+    #                 ediff = 1e-6,
+    #                 #ediffg = -0.01,
+    #                 #kpts = 28,
+    #                 kspacing = 0.16,
+    #                 lcharg = False,
+    #                 lwave = False,
+    #                 isym = 0,
+    #                 ispin = 1,
+    #                 ncore = 8,
+    #                 algo = 'Normal',
+    #                 lreal = 'Auto',
+    #                 #encut = 250,
+    #                 ismear = 1,
+    #                 sigma = 0.2,
+    #                 nsw = 0,
+    #                 ibrion = -1,
+    #                 isif = 2,  # cell changes not yet supported by UFMin
+    #                 #icharg = 1,
+    #                 #lorbit = 11,
+    #                 lasph = True,
+    #                 nelm = 40,
+    #                 #amix = 0.02,
+    #                 #bmix = 0.2,
+    #                 #lmaxmix = 4
+    #                 )
+    #vasp_ntasks = 48
+    #os.environ['ASE_VASP_COMMAND'] = f"mpirun -np {vasp_ntasks} vasp_std"
+    #os.environ['VASP_PP_PATH'] = "/home/graeme/vasp/"  # dir with potpaw
+    #true_calc_type = "vasp"
+
+    ### ==================== ###
 
     tmp = ufmin(initial_structure,
                 live_features_file,
@@ -542,10 +645,12 @@ if __name__ == "__main__":
                 max_uf3_calls,
                 verbose,
                 true_calc,
+                true_calc_type,
                 normalize_forces=normalize_forces,
                 preprocess_strength=preprocess_strength,
                 uq_tolerance=uq_tolerance,
                 pretrained_models=pretrained_models,
+                true_forcecall_always=true_forcecall_always,
                 resume=resume,
                 )
     del tmp
